3cf7f76bd5f267cc9edc3d7db1a0764856c1116e
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2024-03-17 22:45:51 +0100

    fixed walleet connection

--- trace/src/pages/_app.tsx
+++ trace/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import WalletContextProvider from "@/context/WalletContextProvider.jsx";
 import TransitionContextProvider from "@/context/TransitionContextProvider.jsx";
-
+import Navbar from "@/components/Navbar.jsx";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <WalletContextProvider>
+        <Navbar />
+        <ToastContainer />
+
         <TransitionContextProvider>
           <Component {...pageProps} />
         </TransitionContextProvider>
